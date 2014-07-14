32cf0c38b0634d6d6290f11fc3f494809d04deb2
Joel Gombin <joel.gombin@gmail.com>
2014-07-14 22:38:00 +0200

    suppression d'un reactive inutile

--- DRAFTS/TestArchitecture/server.R
+++ DRAFTS/TestArchitecture/server.R
@@ -3,20 +3,18 @@ library(shiny)
 
 shinyServer(function(input, output, session) {
   
-  module <- reactive(input$module)
-  
   # génération des inputs 
   output$inputs <- renderUI({
-    lapply(modules[[module()]][["inputs"]], eval, envir=-2)
+    lapply(modules[[input$module]][["inputs"]], eval, envir=-2)
   })
 
   # serveur proprement dit
-  observe(lapply(modules[[module()]][["server"]], eval, envir=-2))
+  observe(lapply(modules[[input$module]][["server"]], eval, envir=-2))
   
   
   # génération des outputs
   output$outputs <- renderUI({
-    lapply(modules[[module()]][["outputs"]], eval, envir=-2)
+    lapply(modules[[input$module]][["outputs"]], eval, envir=-2)
   })
   
 
