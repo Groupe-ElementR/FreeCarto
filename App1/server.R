5507774c15ce3ad0851d419d7371474b4727f6af
Robin Cura <robin.cura@gmail.com>
2015-03-25 17:53:44 +0100

    adapt Carto to reactiveValues

--- App1/server.R
+++ App1/server.R
@@ -28,45 +28,24 @@ shinyServer(function(input, output, session) {
   
   output$map <- renderPlot({
     par(mar=c(0,0,1.2,0))
-    StaticMap(obj = TNdeleg.spdf, add = FALSE,col = input$couleurFdc, 
+    StaticMap(obj = baseData$spdf, add = FALSE,col = input$couleurFdc, 
               border = input$couleurBorder, lwd = input$epaisseurBorder)
     LayoutMap(title = input$titreCarte, north = input$nord, 
               scale = input$scaleSize, sources = input$sourceMap, 
               author = input$authorMap,
               col = input$colFrame, txtcol = input$colTitle )
-    SymbolsMap(obj = TNdeleg.spdf,title = input$titreLegende,col2 = input$colSymbols2, breakval = input$breakval,
+    SymbolsMap(obj = baseData$spdf,title = input$titreLegende,col2 = input$colSymbols2, breakval = input$breakval,
                pos = input$positionLegende,col = input$couleurSymboles,
-               data = TNdeleg, datavar = input$propVar, 
+               data = baseData$data, datavar = input$propVar, 
                add=TRUE, symbols = input$symboles, k = input$tailleSymbole)
     if(input$labelcheck==TRUE){
-    LabelMap(obj = TNdeleg.spdf, data = TNdeleg, txt = input$labeltxt)
+    LabelMap(obj = baseData$spdf, data = baseData$data, txt = input$labeltxt)
 #     , col = input$labelcol, cex = input$labelcex)
     }
   })
   
 
-  
-  # Compute the hierarchical clustering
-  makeClusters <- reactive({
-    hierarClus <- agnes(selectData(), metric = input$typeDist, method = input$typeCrit)
-    return(hierarClus)
-  })
-  
-  # Make xy plot
-  output$plotxy <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1))
-    clusId <- cutree(makeClusters(), input$nbClus)
-    plot(selectData(),
-         col = clusId,
-         pch = 20, cex = 3)
-  })
-  
-  # Make tree plot
-  output$plottree <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1))
-    hierarTree <- as.dendrogram(makeClusters())
-    plot(hierarTree, leaflab = "none")
-  })
+
   
   # Make the table
   output$userdata <- renderDataTable({
