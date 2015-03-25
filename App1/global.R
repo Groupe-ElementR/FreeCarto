78d21a23eaa231fb58c192aa26c34e69ebdb733b
Robin Cura <robin.cura@gmail.com>
2015-03-25 17:06:05 +0100

    Add basic CAH computing / rendering

--- App1/global.R
+++ App1/global.R
@@ -2,5 +2,38 @@ require(shiny)
 require(freeCarto)
 require(sp)
 require(cluster)
+require(reshape2)
+require(ggplot2)
+
+source("A2R.R")
 load("TNdeleg.RData")
 baseData <- reactiveValues(spdf = TNdeleg.spdf, data = TNdeleg)
+analysisData <- reactiveValues(cah = NA, clusters = NA)
+
+runCAH <- function(df, columns, userDistance , userMethod) {
+  cahDF <- df[,columns]
+  cah <- hclust(dist(cahDF, method = userDistance), method = userMethod)
+  return(cah)
+}
+
+plotProfiles <- function(df, columns, clusters, Cpal, scale){
+  nClusters <- length(unique(clusters))
+  df$CLUSCSP <- factor(clusters, levels = 1:nClusters, labels = paste('Cluster', 1:nClusters, sep = " "))
+  
+  plotDF <- df[, columns]
+  
+  if (scale){plotDF <- sapply(X = plotDF, FUN = function(x) {scale(x, center = TRUE, scale = TRUE)})}
+  
+  clusProfile <- aggregate(plotDF,
+                           by = list(df$CLUSCSP),
+                           mean)
+  
+  colnames(clusProfile)[1] <- "CLUSTER"
+  clusLong <- melt(clusProfile, id.vars = "CLUSTER")
+  ggplot(clusLong) +
+    geom_bar(aes(x = variable, y = value, fill = CLUSTER),
+             stat = "identity") +
+    scale_fill_manual(values = Cpal) +
+    facet_wrap(~ CLUSTER) +
+    coord_flip() + theme_bw()
+}
--- App1/server.R
+++ App1/server.R
@@ -2,6 +2,7 @@
 #### Shiny Server ####
 ######################
 
+require(shiny)
 
 shinyServer(function(input, output, session) {
 
@@ -49,7 +50,42 @@ shinyServer(function(input, output, session) {
   })
   
   output$cahTree <- renderPlot({
-    plot(baseData$data)
+    if (any(!is.na(analysisData$cah))){
+      cah <- analysisData$cah
+      clusters <- analysisData$clusters
+      classOrder <- unique(clusters[cah$order])
+      cPal <- rainbow(n = isolate(input$cahClusters))
+      treePlot <- A2Rplot(x = cah,
+                          k = isolate(input$cahClusters),
+                          boxes = FALSE,
+                          col.up = "gray50",
+                          col.down = cPal[classOrder],
+                          show.labels = FALSE)
+    }
+  })
+  
+  output$cahInertia <- renderPlot({
+    
+    if (any(!is.na(analysisData$cah))){
+      barValues <- cumsum(sort(analysisData$cah$height / sum(analysisData$cah$height), decreasing = TRUE))[1:15] * 100
+      barColors <- rep(x = "white", times = length(barValues))
+      barColors[input$cahClusters] <- "red"
+      barplot(barValues, col = barColors, xlab = "Nombre de classes", ylab = "Part de l'inertie totale (%)",
+              names.arg=1:15, main="Inertie expliquée")
+    }
+  })
+  
+  output$cahProfiles <- renderPlot({
+    if (any(!is.na(analysisData$clusters))){
+      cPal <- rainbow(n = isolate(input$cahClusters))
+      
+      plotProfiles(df = isolate(baseData$data),
+                   columns = isolate(input$cahVariables),
+                   clusters = analysisData$clusters,
+                   Cpal = cPal,
+                   scale = input$cahScale)    
+    }
+  
   })
   
   observe({
@@ -59,5 +95,25 @@ shinyServer(function(input, output, session) {
                       selected = isolate(input$cahVariables),
                       choices = colnames(baseData$data))
   })
+  
+  observe({
+    if (any(!is.null(input$cahVariables) && length(input$cahVariables) > 1)){
+      analysisData$cah <- runCAH(baseData$data, input$cahVariables,
+                                 userDistance = input$cahDistance,
+                                 userMethod = input$cahMethod) 
+    }
+  })
+  
+  observe({
+    if (any(!is.na(analysisData$cah))){
+      analysisData$clusters <- cutree(analysisData$cah, k = input$cahClusters) 
+    }
+  })
+  
+  observeEvent(input$cahAddColumn, {
+    newName <- paste(isolate(input$cahNamePrefix), isolate(input$cahClusters), "Clusters", sep = "_")
+    baseData$data[[newName]] <- isolate(analysisData$clusters)
+  })
+  
   
 })
